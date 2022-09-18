e33c9c3bee3a71be0fddf286afe20e66c1477a54
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-18 08:41:42 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int N = 30;
+int a = 2;
+int b = 7;
+int Method(int x, int y, int num)
+{
+    if (num == 1) return x;
+    if (num == 2) return y;
+    return Method(x, y, num - 1) + Method(x, y, num - 2);
+}
+for (int i = 1; i < N; i++)
+{
+    Console.Write(Method(a, b, i) + " ");
+}
