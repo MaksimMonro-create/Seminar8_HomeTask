19e1051de651ec363c3e8d0c2727fde1dedf3422
MaksimMonro-create <maksim.monro@yandex.ru>
2022-09-18 08:39:35 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.WriteLine(n + " ");
+}
+PrintNumber(6);
